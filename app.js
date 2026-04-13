2b1b0a8bb99eea98ef0143c9a13232fe58576521
Gabriel Nunez <gabrielnunez12@gmail.com>
2026-04-13 12:04:28 +0500

    Add loadTable function to generate verb tables

--- app.js
+++ app.js
@@ -62,6 +62,94 @@ function populateVerbList() {
   });
 }
 
+function loadTable() {
+  const index = document.getElementById("verbSelect").value;
+  const verbObj = data[index];
+
+  currentTable = verbObj;
+
+  let html = `<h2>${verbObj.verb}</h2>`;
+  html += `<table border="1">`;
+
+  // Header
+  html += `
+    <tr>
+      <th>Present</th>
+      <th>Future</th>
+      <th>Past</th>
+      <th>Imperative</th>
+    </tr>
+  `;
+
+  const pronouns = ["я", "ты", "он", "она", "оно", "мы", "вы", "они"];
+  const pastForms = ["masculine", "feminine", "neuter", "plural"];
+  const imperativeForms = ["singular", "plural"];
+
+  const maxRows = 8; // longest column
+
+  for (let i = 0; i < maxRows; i++) {
+    html += "<tr>";
+
+    // PRESENT
+    if (i < pronouns.length) {
+      const p = pronouns[i];
+      html += `
+        <td>
+          ${p}<br>
+          <input data-tense="present" data-pronoun="${p}">
+        </td>
+      `;
+    } else {
+      html += "<td></td>";
+    }
+
+    // FUTURE
+    if (i < pronouns.length) {
+      const p = pronouns[i];
+      html += `
+        <td>
+          ${p}<br>
+          <input data-tense="future" data-pronoun="${p}">
+        </td>
+      `;
+    } else {
+      html += "<td></td>";
+    }
+
+    // PAST
+    if (i < pastForms.length) {
+      const form = pastForms[i];
+      html += `
+        <td>
+          ${form}<br>
+          <input data-tense="past" data-pronoun="${form}">
+        </td>
+      `;
+    } else {
+      html += "<td></td>";
+    }
+
+    // IMPERATIVE
+    if (i < imperativeForms.length) {
+      const form = imperativeForms[i];
+      html += `
+        <td>
+          ${form}<br>
+          <input data-tense="imperative" data-pronoun="${form}">
+        </td>
+      `;
+    } else {
+      html += "<td></td>";
+    }
+
+    html += "</tr>";
+  }
+
+  html += "</table>";
+
+  document.getElementById("tableContainer").innerHTML = html;
+}
+
 document.getElementById("answer")
   .addEventListener("keypress", function(e) {
     if (e.key === "Enter") {
