f105763f99a19c01c8f1bcd72cf53a5b9a409050
Gabriel Nunez <gabrielnunez12@gmail.com>
2026-05-25 00:14:02 +0100

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,1 +1,39 @@
+function updateURL() {
 
+  const params = new URLSearchParams();
+
+  if (appState.cat !== "all") {
+    params.set("cat", appState.cat);
+  }
+  
+  if (appState.aspect !== "any") {
+    params.set("aspect", appState.aspect);
+  }
+  
+  if (appState.conjugation !== "any") {
+    params.set("conjugation", appState.conjugation);
+  }
+  
+  if (appState.stem !== "any") {
+    params.set("stem", appState.stem);
+  }
+  
+  if (appState.class !== "any") {
+    params.set("class", appState.class);
+  }
+  
+  if (appState.search !== "") {
+    params.set("search", appState.search);
+  }
+
+  if (appState.selectedVerb) {
+    params.set("verb", appState.selectedVerb);
+  }
+  
+  const query = params.toString();
+
+  window.location.hash =
+    query
+      ? `tableTrainer?${query}`
+      : "tableTrainer";
+}
